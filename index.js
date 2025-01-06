a286f4cf53759fa4de46a83ffa2c9e961a02fcab
Ignacio Jorquera <ijorquera@alumnos.uai.cl>
2025-01-06 14:04:44 -0300

    Refactor HTML structure and CSS styles for improved layout and accessibility; add alt attributes for images in JavaScript.

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21492
+        likes: 21492,
+        avatarAlt: "Profile image of Vincent van Gogh.",
+        postAlt: "Self-portrait by Vincent van Gogh."
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +17,9 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        avatarAlt: "Profile image of Gustave Courbet.",
+        postAlt: "The Desperate Man by Gustave Courbet."
     },
         {
         name: "Joseph Ducreux",
@@ -24,7 +28,9 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        avatarAlt: "Profile image of Joseph Ducreux.",
+        postAlt: "Portrait de l'artiste sous les traits d'un moqueur, Self-portrait by Joseph Ducreux."
     }
 ]
 
@@ -33,15 +39,15 @@ const containerEl = document.getElementById("container")
 for (let i = 0; i < posts.length; i++) {
     containerEl.innerHTML += 
         `<header>
-            <img class="user-img" src="${posts[i].avatar}" alt="Profile image of Vincent van Gogh.">
+            <img class="user-img" src="${posts[i].avatar}" alt="${posts[i].avatarAlt}">
             <div class="user-info">
                 <p class="bold-text mg-zero">${posts[i].name}</p>
                 <p class="mg-zero">${posts[i].location}</p>
             </div>
         </header>
-        <main>
-            <img class="main-img" src="${posts[i].post}" alt="Self-portrait by Vincent van Gogh."></img>
-        </main>
+        <section id="img-section" class="mg-zero">
+            <img class="main-img" src="${posts[i].post}" alt="${posts[i].postAlt}"></img>
+        </section>
         <section>
             <div class="icons">
                 <a id="like-btn-${i}"><img class="icon" src="images/like.png" alt="Heart icon."></a>
